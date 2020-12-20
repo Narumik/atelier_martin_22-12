cf4b7b1b580167a77477fd5d1674e94047bdb96d
Narumik <contact.narumik@gmail.com>
2020-12-20 12:39:59 +0100

    WIP Movement Script

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    [SerializeField] private float speed = 1;
+    public float HorizontalSpeed = 1;
+    public float VerticalSpeed = 1;
+
+    void FixedUpdate()
+
+    {
+
+        float moveHorizontal = Input.GetAxis("Vertical") * HorizontalSpeed;
+        float moveVertical = = Input.GetAxis("Horizontal") * VerticalSpeed;
+        Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+        GetComponent<Rigidbody>().AddForce(movement*speed*Time.deltaTime);
+    }
 }
