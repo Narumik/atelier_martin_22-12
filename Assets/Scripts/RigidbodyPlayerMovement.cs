19fde0670e28e66229f4690dd182240e047e321f
Narumik <contact.narumik@gmail.com>
2020-12-20 20:13:47 +0100

    Rigidbody Movement script

---
+++ Assets/Scripts/RigidbodyPlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyPlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody playerBody;
+
+    private Vector3 inputVector;
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+        inputVector = new Vector3(Input.GetAxis("Horizontal")*10, playerBody.velocity.y, Input.GetAxis("Vertical")*10);
+        transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+    }
+
+    private void FixedUpdate()
+    {
+        playerBody.velocity = inputVector;
+    }
+
+}
