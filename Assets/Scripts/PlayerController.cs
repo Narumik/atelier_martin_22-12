315764279f5d0261598effda2c25902475143c83
Narumik <contact.narumik@gmail.com>
2020-12-20 12:09:32 +0100

    Backup be4 obsolete API

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,14 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+        rigidbody.Addforce(movement*speed*Time.deltaTime);
+
     }
 }
