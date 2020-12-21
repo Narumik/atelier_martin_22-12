65edb1f5e3678b4c8c20b8ead1ae8ddde7c4cb21
Narumik <contact.narumik@gmail.com>
2020-12-21 14:51:42 +0100

    Attempt scripts fusion

--- Assets/Scripts/RigidbodyPlayerMovement.cs
+++ Assets/Scripts/RigidbodyPlayerMovement.cs
@@ -7,6 +7,11 @@ public class RigidbodyPlayerMovement : MonoBehaviour
     [SerializeField]
     private Rigidbody playerBody;
 
+    public CharacterController controller;
+    public Transform cam;
+
+    
+
     private Vector3 inputVector;
     void Start()
     {
@@ -18,6 +23,11 @@ public class RigidbodyPlayerMovement : MonoBehaviour
         
         inputVector = new Vector3(Input.GetAxis("Horizontal")*10, playerBody.velocity.y, Input.GetAxis("Vertical")*10);
         transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+
+        float targetAngle = Mathf.Atan2(inputVector.x, inputVector.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+        Vector3 moveDir = Quaternion.Euler(0f, targetAngle,0f)*Vector3.forward;
+        controller.Move(moveDir.normalized*6f*Time.deltaTime);
     }
 
     private void FixedUpdate()
