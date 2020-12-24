4aa5e71ab675b764bd5071c7bfc91eb215071ecb
Narumik <contact.narumik@gmail.com>
2020-12-24 19:06:09 +0100

    Quasi finiii

---
+++ Assets/Scripts/PlayBarrierSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBarrierSound : MonoBehaviour
+{
+    public AudioSource barrierSound;
+
+    void OnTriggerEnter(Collider target)
+    {
+        if(target.tag == "Floor"){
+            barrierSound.Play();
+        }
+    }
+}
